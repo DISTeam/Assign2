317e2250c662b1be980cab289d0e1cf32434ac3b
ManoBhargav D <manobhargav.531@gmail.com>
2018-10-01 02:59:34 -0400

    code using while

--- Assn2_StockList_Students_3.cs
+++ Assn2_StockList_Students_3.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Assignment_2
@@ -23,12 +24,15 @@ namespace Assignment_2
                 StockNode items = this.head;
                 value = value + items.StockHolding.Holdings * items.StockHolding.CurrentPrice;
 
-                do
+              
+     
+                while (items.Next != null)
                 {
                     items = items.Next;
-                    value = value + items.StockHolding.Holdings * items.StockHolding.CurrentPrice;
+                   value = value + items.StockHolding.Holdings * items.StockHolding.CurrentPrice;
                 }
-                while (items.Next != null);
+
+
 
 
 
@@ -45,9 +49,45 @@ namespace Assignment_2
     {
       int similarityIndex = 0;
 
-      // write your implementation here
+            // write your implementation here
 
-      return similarityIndex;
+            var stock1 = new List<string>();
+
+            if (!this.IsEmpty())
+            {
+
+                StockNode items = this.head;
+                stock1.Add(items.StockHolding.Symbol);
+
+             
+                while (items.Next != null)
+                {
+                    items = items.Next;
+                    stock1.Add(items.StockHolding.Symbol);
+                }
+            }
+
+            var stock2 = new List<string>();
+
+            if (!listToCompare.IsEmpty())
+            {
+                var items = new StockNode();
+                items = listToCompare.head;
+                stock2.Add(items.StockHolding.Symbol);
+
+              
+                while (items.Next != null)
+                {
+                    items = items.Next;
+                    stock2.Add(items.StockHolding.Symbol);
+                };
+            }
+
+            var CommonList = stock1.Intersect(stock2);
+            similarityIndex = CommonList.Count();
+
+
+            return similarityIndex;
     }
 
     //param        : NA
@@ -56,10 +96,22 @@ namespace Assignment_2
     //return type  : NA
     public void Print()
     {
-      // write your implementation here
+            // write your implementation here
 
-			
+            if (!this.IsEmpty())
+            {
+                StockNode items = this.head;
+                Console.WriteLine(items.StockHolding.ToString());
 
-    }
+              
+                while (items.Next != null)
+                {
+                    items = items.Next;
+                    Console.WriteLine(items.StockHolding.ToString());
+                }
+
+            }
+
+        }
   }
 }
